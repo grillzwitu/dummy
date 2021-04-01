009a02d5054f73167fa80b227b6d619c242e4321
grillzwitu <grillzwitu@gmail.com>
2021-04-01 16:32:28 +0000

    simple_shell resources tasks as I progress

---
+++ simple_shell/0_av.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	int i;
+
+	(void)ac;
+
+	for (i = 0; av[i] != '\0'; i++)
+	{
+		printf("%s\n", av[i]);
+	}
+	return (0);
+}
---
+++ simple_shell/prompt.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Prompts for string input and prints it in stdout
+ *
+ * Return: 0(Success)
+ */
+
+int main()
+{
+	char *buffer;
+	size_t bufsize = 32;
+	size_t characters;
+
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
+		exit(1);
+	}
+
+	printf("$: ");
+	characters = getline(&buffer,&bufsize,stdin);
+	/*printf("%u characters were read.\n", (unsigned) sizeof(characters));*/
+	printf("%s",buffer);
+	(void) characters;
+
+	free(buffer);
+	return(0);
+}
